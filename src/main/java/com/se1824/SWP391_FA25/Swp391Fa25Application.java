4fa09caae66732191d98000fdd0b21b718f55ef5
viethoangfpthcm <phanviethoangfpthcm@gmail.com>
2025-09-24 14:57:19 +0700

    first commit

---
+++ src/main/java/com/se1824/SWP391_FA25/Swp391Fa25Application.java
@@ -0,0 +1,13 @@
+package com.se1824.SWP391_FA25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391Fa25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391Fa25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/se1824/SWP391_FA25/Swp391Fa25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se1824.SWP391_FA25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391Fa25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
