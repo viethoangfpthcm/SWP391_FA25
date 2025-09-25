c40ddb3973d93b602feec0c43bb2d68ed2127c52
gnouv-fpt <228469372+gnouv-fpt@users.noreply.github.com>
2025-09-25 21:09:01 +0700

    Push entity User

---
+++ src/main/java/com/se1824/SWP391_FA25/entity/User.java
@@ -0,0 +1,27 @@
+package com.se1824.SWP391_FA25.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jdk.jfr.Enabled;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    UUID id;
+    String username;
+    String password;
+    String fullname;
+    String email;
+    String phone;
+    String role;
+    boolean is_active;
+    LocalDateTime created_at;
+
+}
