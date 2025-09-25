d40a5014916d98106718dbce353e4e98b3c283c3
gnouv-fpt <228469372+gnouv-fpt@users.noreply.github.com>
2025-09-25 21:09:36 +0700

    Push entity User

--- src/main/java/com/se1824/SWP391_FA25/entity/User.java
+++ src/main/java/com/se1824/SWP391_FA25/entity/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jdk.jfr.Enabled;
+
 import lombok.Data;
 
 import java.time.LocalDateTime;
