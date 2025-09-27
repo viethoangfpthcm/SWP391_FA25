5b536660e92ed99dd6e4f2282c04afe93590c720
gnouv-fpt <228469372+gnouv-fpt@users.noreply.github.com>
2025-09-27 09:49:00 +0700

    Update login method & PasswordEncoder

--- src/main/java/com/se1824/SWP391_FA25/service/AuthenticationService.java
+++ src/main/java/com/se1824/SWP391_FA25/service/AuthenticationService.java
@@ -3,8 +3,11 @@ package com.se1824.SWP391_FA25.service;
 import com.se1824.SWP391_FA25.entity.User;
 import com.se1824.SWP391_FA25.model.response.UserResponse;
 import com.se1824.SWP391_FA25.repository.AuthenticationRepository;
+import io.jsonwebtoken.security.Password;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,8 @@ public class AuthenticationService {
     AuthenticationRepository authenticationRepository;
     @Autowired
     ModelMapper modelMapper;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
 
     public UserResponse getUserByEmail(String email) {
@@ -25,6 +30,14 @@ public class AuthenticationService {
     }
 
     public User registerUser(User user) {
+        user.setPasswordHash(passwordEncoder.encode(user.getPasswordHash()));
         return authenticationRepository.save(user);
+    }
+    public UserResponse login (String email, String rawPassword) {
+        User user = authenticationRepository.findByEmail(email);
+        if (user == null || !passwordEncoder.matches(rawPassword, user.getPasswordHash())) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        return modelMapper.map(user, UserResponse.class);
     }
 }
