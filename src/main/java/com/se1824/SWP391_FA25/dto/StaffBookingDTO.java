4ca05fd8f293c062c5027a3557d52cfb4a8f2880
gnouv-fpt <228469372+gnouv-fpt@users.noreply.github.com>
2025-10-05 15:04:40 +0700

    Add StaffBookingDTO & StaffService

---
+++ src/main/java/com/se1824/SWP391_FA25/dto/StaffBookingDTO.java
@@ -0,0 +1,23 @@
+package com.se1824.SWP391_FA25.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class StaffBookingDTO {
+    Integer bookingId;
+     String customerName;
+     String customerPhone;
+     String vehiclePlate;
+     String vehicleModel;
+     Integer currentKm;
+     LocalDateTime bookingDate;
+     String status;
+     String note;
+     String centerName;
+}
--- src/main/java/com/se1824/SWP391_FA25/repository/UserRepository.java
+++ src/main/java/com/se1824/SWP391_FA25/repository/UserRepository.java
@@ -5,7 +5,11 @@ import com.se1824.SWP391_FA25.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, String> {
-
+    Users findUserByEmail(String email);
+    List<Users> findByUserIdStartingWith(String prefix);
+    List<Users> findByServiceCenter_IdAndUserIdStartingWith(Integer centerId, String prefix);
 }
