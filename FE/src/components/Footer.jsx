d38e028e230eff07f3aa421ea06b0dcd0bc579bf
giabaone281005 <khazim2810@gmail.com>
2025-10-16 13:37:12 +0700

    update css

--- FE/src/components/Footer.jsx
+++ FE/src/components/Footer.jsx
@@ -1,32 +1,48 @@
 import React from "react";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-column">
-        <h3>EV Car Center</h3>
-        <p>231 Nguyễn Văn Ngân, Thủ Đức, TP.HCM</p>
-        <p>Hotline: 0787052810</p>
-        <p>Email: EVCarCenter@gmail.com</p>
-      </div>
+      <div className="footer-top">
+        <div className="footer-column">
+          <h3 className="footer-title">EV Car Center</h3>
+          <p>231 Nguyễn Văn Ngân, Thủ Đức, TP.HCM</p>
+          <p>Hotline: <strong>0787 052 810</strong></p>
+          <p>Email: <a href="mailto:EVCarCenter@gmail.com">EVCarCenter@gmail.com</a></p>
+        </div>
 
-      <div className="footer-column">
-        <h3>Sản phẩm</h3>
-        <p><a href="#">Bảo dưỡng xe</a></p>
-        <p><a href="#">Đặt lịch bảo dưỡng</a></p>
-        <p><a href="#">Trung tâm gần nhất</a></p>
-      </div>
+        <div className="footer-column">
+          <h4>Dịch vụ</h4>
+          <ul>
+            <li><a >Bảo dưỡng xe</a></li>
+            <li><a >Đặt lịch bảo dưỡng</a></li>
+            <li><a >Trung tâm gần nhất</a></li>
+          </ul>
+        </div>
 
-      <div className="footer-column">
-        <h3>Kết nối</h3>
-        <p><a href="#">Facebook</a></p>
-        <p><a href="#">Instagram</a></p>
-        <p><a href="#">Twitter</a></p>
+        <div className="footer-column">
+          <h4>Về chúng tôi</h4>
+          <ul>
+            <li><a >Giới thiệu</a></li>
+            <li><a >Tin tức & Khuyến mãi</a></li>
+            <li><a >Liên hệ</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h4>Kết nối với chúng tôi</h4>
+          <div className="footer-socials">
+            <a href="#"><FaFacebookF /></a>
+            <a href="#"><FaInstagram /></a>
+            <a href="#"><FaTwitter /></a>
+          </div>
+        </div>
       </div>
 
       <div className="footer-bottom">
-        © 2025 EV Car Center. All rights reserved.
+        © 2025 <strong>EV Car Center</strong>. All rights reserved.
       </div>
     </footer>
   );
