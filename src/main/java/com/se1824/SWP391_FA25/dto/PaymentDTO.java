69a9315a0b45c7ab620172f2a59065284b03f183
gnouv-fpt <228469372+gnouv-fpt@users.noreply.github.com>
2025-10-24 23:45:25 +0700

    Update alternative flow for admin

---
+++ src/main/java/com/se1824/SWP391_FA25/dto/PaymentDTO.java
@@ -0,0 +1,24 @@
+package com.se1824.SWP391_FA25.dto;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PaymentDTO {
+    Integer paymentId;
+    Integer bookingId;
+    LocalDateTime paymentDate;
+    BigDecimal laborCost;
+    BigDecimal materialCost;
+    BigDecimal totalAmount;
+    String paymentMethod;
+    String status;
+    String note;
+    Integer centerId;
+    String centerName;
+}
