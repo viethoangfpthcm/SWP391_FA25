2d4f91857e571b78daa1bd94336e8e888d28d851
Tran Nam Khanh <khanhtrangamer@gmail.com>
2025-11-05 23:06:51 +0700

    SACH SE

--- FE/src/components/layout/Navbar.jsx
+++ FE/src/components/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "/public/bo may lay.png";
+import logo from "/bo may lay.png";
 import "./Navbar.css";
 import Button from "@components/ui/Button.jsx";
 
